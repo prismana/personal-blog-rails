db3838958909fdc15e88298583165a711d0cd2ca
Rahdriawan Prismana <63779468+prismana@users.noreply.github.com>
2026-06-09 04:05:15 +0000

    Adding destroy blog post in router, controller. Refactoring some of routing and method in controller

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,11 +1,12 @@
 class BlogPostsController < ApplicationController
+    # Helper untuk find by id
+    before_action :set_blog_post, only: [:show, :edit, :update, :destroy]
 
     def index
         @blog_posts = BlogPost.all
     end
 
     def show
-        @blog_post = BlogPost.find(params[:id])
     rescue ActiveRecord::RecordNotFound
         redirect_to root_path
     end
@@ -27,12 +28,9 @@ class BlogPostsController < ApplicationController
 
     # Edit and update
     def edit
-        @blog_post = BlogPost.find(params[:id])
     end
 
     def update
-        @blog_post = BlogPost.find(params[:id])
-
         if @blog_post.update(blog_post_params)
             redirect_to @blog_post
         else
@@ -40,10 +38,23 @@ class BlogPostsController < ApplicationController
         end
     end
 
+    # Destroy
+    def destroy
+        @blog_post.destroy
+
+        redirect_to root_path
+    end
+
     private
     
     def blog_post_params
         params.require(:blog_post).permit(:title, :body)
+    end
+
+    def set_blog_post
+        @blog_post = BlogPost.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+        redirect_to root_path
     end
 
 end
